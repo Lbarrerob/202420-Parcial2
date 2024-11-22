b3f5cd780a6f9ba40fefe4606e9421a11481cef0
Lbarrerob <l.barrerob@uniandes.edu.co>
2024-11-22 07:51:23 -0500

    .

--- pokemon/src/environments/environment.development.ts
+++ pokemon/src/environments/environment.development.ts
@@ -1,1 +1,6 @@
-export const environment = {};
+const baseUrl = 'https://raw.githubusercontent.com/2603-Uniandes/jsons/refs/heads/main/202420%20Pokemon/';
+
+export const environment = {
+  production: true,
+  baseUrl
+};
